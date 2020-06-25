13f2ff8653d85446b9b2f31515b383841fe0ce7e
YuriiTrofimenko <tyaa@ukr.net>
2020-06-25 09:38:44 +0300

    init

---
+++ src/test/java/org/tyaa/demo/springboot/simplespa/SpringbootSimplespaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.springboot.simplespa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimplespaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
