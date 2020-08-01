897f35f707ed6d814829461404ec1808278b6a7d
YuriiTrofimenko <tyaa@ukr.net>
2020-08-01 15:17:47 +0300

    setup materialize-css with react-materialize

--- src/main/webapp/app/components/App.tsx
+++ src/main/webapp/app/components/App.tsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
+import {Container, Toast} from 'react-materialize'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min'
 
 class App extends Component {
     render () {
-        return <h1>Hello SpringBoot from ReactJs!</h1>
+        return <Container>
+            <h1>Hello SpringBoot from ReactJs with MaterializeCss!</h1>
+            <Toast
+                options={{
+                    html: 'MaterializeCss Js Works!'
+                }}
+            >
+                Test MaterializeCss Js
+            </Toast>
+        </Container>
     }
 }
 
